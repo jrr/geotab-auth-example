36cf56739a42bae122b752e6a6a2fe02a8c7e7d3
John Ruble <johnruble@gmail.com>
2017-09-09 02:43:16 -0400

    connect four different ways

--- Program.cs
+++ Program.cs
@@ -39,14 +39,59 @@ namespace geotab_authentication
             }
 
             string sessionId = null;
-            var httpHandler = new CountingHttpHandler();
 
-            var api = new Geotab.Checkmate.API(fleetInfo.Username,fleetInfo.Password, sessionId,fleetInfo.GeotabDatabase, handler: httpHandler);
-            api.Authenticate();
+            sessionId = Example("Connecting with only password",
+                username: fleetInfo.Username,
+                password: fleetInfo.Password,
+                database: fleetInfo.GeotabDatabase,
+                sessionId: null);
 
-            var devices = api.Call<List<Device>>("Get", typeof(Device));
-            Console.WriteLine($"got {devices.Count} devices.");
+            sessionId = Example("Reusing sessionId with new API object (no password, no Authenticate())",
+                username: fleetInfo.Username,
+                password: null,
+                database: fleetInfo.GeotabDatabase,
+                sessionId: sessionId,
+                authenticate: false);
 
+            sessionId = Example("Connecting with both password and sessionId",
+                username: fleetInfo.Username,
+                password: fleetInfo.Password,
+                database: fleetInfo.GeotabDatabase,
+                sessionId: sessionId);
+
+            sessionId = Example("Reusing sessionId with new API object (no password, calling Authenticate())",
+                username: fleetInfo.Username,
+                password: null,
+                database: fleetInfo.GeotabDatabase,
+                sessionId: sessionId,
+                authenticate: true);
+        }
+
+        private static string Example(string description, string username, string password, string database, string sessionId, bool authenticate = true)
+        {
+            Console.WriteLine($"\n\n#\n#\t{description}\n#\n");
+            var api = new Geotab.Checkmate.API(
+                userName: username,
+                password: password,
+                database: database,
+                sessionId: sessionId,
+                handler: new LoggingHttpHandler()
+            );
+
+            try
+            {
+                if (authenticate) api.Authenticate();
+
+                var devices = api.Call<List<Device>>("Get", typeof(Device));
+
+                Console.WriteLine($"\n\tGot {devices.Count} devices.");
+            }
+            catch (Geotab.Checkmate.ObjectModel.InvalidUserException e)
+            {
+                Console.WriteLine($"\n\tGeotab Exception: {e.Message}");
+            }
+
+            return api.SessionId;
         }
 
         public struct GeotabFleetInformation
@@ -57,22 +102,25 @@ namespace geotab_authentication
         }
     }
 
-    internal class CountingHttpHandler : System.Net.Http.MessageProcessingHandler
+    internal class LoggingHttpHandler : System.Net.Http.MessageProcessingHandler
     {
-        public CountingHttpHandler(){
+        public LoggingHttpHandler()
+        {
             this.InnerHandler = new System.Net.Http.HttpClientHandler();
         }
         protected override HttpRequestMessage ProcessRequest(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var content = request.Content.ReadAsStringAsync().Result;
-            Console.WriteLine($"Request {request.Method} {request.RequestUri} {content}");
+            Console.WriteLine($"\tRequest {request.Method} {request.RequestUri} {content}");
             return request;
         }
 
         protected override HttpResponseMessage ProcessResponse(HttpResponseMessage response, CancellationToken cancellationToken)
         {
             var content = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine($"Reponse {response.StatusCode}");
+            var extra = "";
+            if (content.Contains("error")) extra = content;
+            Console.WriteLine($"\tReponse {response.StatusCode} {extra}");
             return response;
         }
     }
