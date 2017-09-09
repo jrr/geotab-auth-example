f06700e334d6eb047fd77463d675a373d1c60cc6
John Ruble <johnruble@gmail.com>
2017-09-09 01:25:15 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace geotab_authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
